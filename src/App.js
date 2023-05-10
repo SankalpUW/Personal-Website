f1a240f96f8f942874262d788b7314ef955d55ec
Sankalp Kodandera <skodande@uwaterloo.ca>
2023-05-09 23:05:35 -0400

    website

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import './App.css';
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import Home from './pages/Home';
-import Resume from './pages/Resume';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 
