95febb20b84c5d5fe08aec780a28450ff5509be8
Sankalp Kodandera <skodande@uwaterloo.ca>
2023-05-09 22:58:00 -0400

    website

--- src/pages/Resume.js
+++
@@ -1,9 +0,0 @@
-import GithubIcon from "@material-ui/icons/GitHub";
-
-function Resume(){
-    return (
-        <a href="https://github.com/SankalpUW" target="_blank"></a>
-    );
-}
-
-export default Resume;
